97b9bf1452498465bc96a2d00241f1cc32893715
Aseel <aseelmahmood12@gmail.com>
2025-02-06 22:54:27 +0100

    Initial Commit

---
+++ E-FOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  E-FOS
+//
+//  Created by Aseel Yousif on 2/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ E-FOS/E_FOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_FOSApp.swift
+//  E-FOS
+//
+//  Created by Aseel Yousif on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct E_FOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
