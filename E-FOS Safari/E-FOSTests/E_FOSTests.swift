d4e1ebc5beabfa1b3d4afd3cc8dc3b3242067c23
Aseel <aseelmahmood12@gmail.com>
2025-02-07 21:37:03 +0100

    Safari extension base structure

---
+++ E-FOS Safari/E-FOSTests/E_FOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  E_FOSTests.swift
+//  E-FOSTests
+//
+//  Created by Aseel Yousif on 2/7/25.
+//
+
+import Testing
+@testable import E_FOS
+
+struct E_FOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
