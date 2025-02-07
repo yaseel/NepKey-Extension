5f06ffac985d47faea938174a2ae318296808081
Aseel <aseelmahmood12@gmail.com>
2025-02-07 01:10:09 +0100

    Added neptun, canvas, tms buttons

--- E-FOS MacOS/E-FOS/ContentView.swift
+++ E-FOS MacOS/E-FOS/ContentView.swift
@@ -1,19 +1,54 @@
-//
-//  ContentView.swift
-//  E-FOS
-//
-//  Created by Aseel Yousif on 2/6/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("E-FOS")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+
+            HStack(spacing: 40) {
+                VStack {
+                    Link(destination: URL(string: "https://neptun.elte.hu")!) {
+                        VStack {
+                            Image("neptun_icon")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                            Text("Neptun")
+                                .font(.headline)
+                        }
+                    }
+                }
+
+                VStack {
+                    Link(destination: URL(string: "https://canvas.elte.hu")!) {
+                        VStack {
+                            Image("canvas_icon")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                            Text("Canvas")
+                                .font(.headline)
+                        }
+                    }
+                }
+
+                VStack {
+                    Link(destination: URL(string: "https://tms.inf.elte.hu")!) {
+                        VStack {
+                            Image("tms_icon")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                            Text("TMS")
+                                .font(.headline)
+                        }
+                    }
+                }
+            }
+            .padding()
         }
         .padding()
     }
--- E-FOS MacOS/E-FOS/E_FOSApp.swift
+++ E-FOS MacOS/E-FOS/E_FOSApp.swift
@@ -1,17 +1,44 @@
-//
-//  E_FOSApp.swift
-//  E-FOS
-//
-//  Created by Aseel Yousif on 2/6/25.
-//
-
 import SwiftUI
 
 @main
 struct E_FOSApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView() // No main window
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
+    var popover: NSPopover!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        print("App launched") // Debug log
+
+        popover = NSPopover()
+        popover.contentSize = NSSize(width: 300, height: 400)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: ContentView())
+        print("Popover created") // Debug log
+
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "star", accessibilityDescription: "E-FOS")
+            button.action = #selector(togglePopover(_:))
+        }
+    }
+
+
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = statusItem.button {
+            if popover.isShown {
+                popover.performClose(sender)
+            } else {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            }
         }
     }
 }
