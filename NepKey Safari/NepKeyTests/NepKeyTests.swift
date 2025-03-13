572c879675adb32644be79f9c8a53b4b5ea9db41
Aseel <aseelmahmood12@gmail.com>
2025-03-13 21:00:28 +0100

    EFOS to NepKey renaming

---
+++ NepKey Safari/NepKeyTests/NepKeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  NepKeyTests.swift
+//  NepKeyTests
+//
+//  Created by Aseel Yousif on 2/7/25.
+//
+
+import Testing
+@testable import NepKey
+
+struct NepKeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
