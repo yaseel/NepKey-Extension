dcd60b2f8701f0591dc49da1b9340dd2a11b3276
Aseel <aseelmahmood12@gmail.com>
2025-07-19 01:32:08 +0200

    Font added, extra cleanup

--- nepkey-react/src/App.tsx
+++ nepkey-react/src/App.tsx
@@ -1,12 +1,13 @@
-import "./App.css";
 import {
+    Route,
     Routes
 } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
 
 function App() {
     return (
         <Routes>
-
+            <Route path="/" element={<HomePage/>}/>
         </Routes>
     );
 }
--- nepkey-react/src/pages/HomePage.tsx
+++ nepkey-react/src/pages/HomePage.tsx
@@ -3,12 +3,9 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>NepKey</h1>
-      <Link to="/settings">
-        <button>Go to Settings</button>
-      </Link>
-    </div>
+    <>
+      Hi
+    </>
   );
 };
 
