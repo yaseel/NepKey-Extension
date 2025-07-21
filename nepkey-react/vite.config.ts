b4dc4805244757b66564d04837d2e7efbe9eaf62
Aseel <aseelmahmood12@gmail.com>
2025-07-21 20:27:13 +0200

    Setup build for extension

--- nepkey-react/vite.config.ts
+++ nepkey-react/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'background.ts'),
+        content: resolve(__dirname, 'content.ts'),
+      },
+      output: {
+        entryFileNames: chunk => {
+          if (chunk.name === 'background' || chunk.name === 'content') {
+            return '[name].js';
+          }
+          return 'assets/[name]-[hash].js';
+        },
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
